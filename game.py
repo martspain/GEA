de32efdf6753b87d105b18df057538ab933da9e0
Martín España <46570760+martspain@users.noreply.github.com>
2023-11-26 04:42:33 -0600

    Added collision detection

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from spritesheet import SpriteSheet
 
 class Game:
@@ -14,6 +15,12 @@ class Game:
     self.playerHeight = 8
     self.playerScale = 5
     self.currentMovement = []
+
+    self.cannonPos = (70, 350)
+    self.cannonColor = (50,50,150)
+    self.cannonBallPos = (self.cannonPos[0], self.cannonPos[1])
+    self.cannonBallColor = (50,50,50)
+    self.cannonBallVelocity = 3
 
     self.runningGame = False
     self.mainCharacterSpritesheet = None
@@ -35,6 +42,35 @@ class Game:
     if self.currentMovement.count("up") > 0:
       if self.playerPosition[1] - self.playerVelocity >= 0:
         self.playerPosition = (self.playerPosition[0], self.playerPosition[1] - self.playerVelocity)
+
+  def update_cannonBall_position(self):
+    if self.cannonBallPos[0] >= self.width:
+      # Reset cannonball
+      self.cannonBallPos = (self.cannonPos[0], self.cannonPos[1])
+    else:
+      self.cannonBallPos = (self.cannonBallPos[0] + self.cannonBallVelocity, self.cannonBallPos[1])
+
+  # Check collision between rect and circle
+  # Reference: https://stackoverflow.com/questions/24727773/detecting-rectangle-collision-with-a-circle
+  def rect_circle_collision(self, rleft, rtop, width, height, center_x, center_y, radius):
+
+    rright, rbottom = rleft + width/2, rtop + height/2
+
+    cleft, ctop = center_x-radius, center_y-radius
+    cright, cbottom = center_x+radius, center_y+radius
+
+    if rright < cleft or rleft > cright or rbottom < ctop or rtop > cbottom:
+      return False
+
+    for x in (rleft, rleft+width):
+      for y in (rtop, rtop+height):
+        if math.hypot(x-center_x, y-center_y) <= radius:
+          return True
+
+    if rleft <= center_x <= rright and rtop <= center_y <= rbottom:
+      return True
+
+    return False
 
 
   def run(self):
@@ -85,6 +121,12 @@ class Game:
 
       self.update_player_position()
 
+      if self.rect_circle_collision(self.playerPosition[0], self.playerPosition[1], self.playerWidth * self.playerScale, self.playerHeight * self.playerScale, self.cannonBallPos[0], self.cannonBallPos[1], 10):
+        print("GAME OVER - Colision Detectada")
+        self.runningGame = False
+      else:
+        self.update_cannonBall_position()
+
       if currentTime - last_update >= animation_cooldown:
         if bg_currentFrame < len(bg_animation)-1:
           bg_currentFrame += 1
@@ -97,6 +139,10 @@ class Game:
         last_update = currentTime
 
       screen.blit(bg_animation[bg_currentFrame], (0, 0))
+
+      pygame.draw.rect(screen, self.cannonColor, pygame.Rect(self.cannonPos[0], self.cannonPos[1], 40, 40))
+      pygame.draw.circle(screen, self.cannonBallColor, (self.cannonBallPos[0] + 20, self.cannonBallPos[1] + 20), 10)
+      
       screen.blit(animations[self.currentState][currentFrame], self.playerPosition)
       
       # Handle Events
